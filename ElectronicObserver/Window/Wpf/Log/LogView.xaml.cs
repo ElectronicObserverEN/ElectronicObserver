9d2a1207fee30cdb1fc7be975ccda09755fc576d
gre4bee <gre4bee@gmail.com>
2022-07-08 22:49:58 +0800

    Additional code cleanup and simplify scroling code

--- ElectronicObserver/Window/Wpf/Log/LogView.xaml.cs
+++ ElectronicObserver/Window/Wpf/Log/LogView.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Media;
+using ModernWpf.Controls;
 
 namespace ElectronicObserver.Window.Wpf.Log;
 /// <summary>
@@ -24,37 +26,12 @@ public partial class LogView : UserControl
 		InitializeComponent();
 	}
 
-	private void ListBox_OnLoaded(object sender, RoutedEventArgs e)
+	private void ListBox_ScrollChanged(object sender, ScrollChangedEventArgs e)
 	{
-		var listBox = (ListBox)sender;
-
-		var scrollViewer = FindScrollViewer(listBox);
-
-		if (scrollViewer != null)
+		if (e.OriginalSource is ScrollViewer scrollViewer &&
+		Math.Abs(e.ExtentHeightChange) > 0.0)
 		{
-			scrollViewer.ScrollChanged += (o, args) =>
-			{
-				if (args.ExtentHeightChange > 0)
-					scrollViewer.ScrollToBottom();
-			};
+			scrollViewer.ScrollToBottom();
 		}
-	}
-	// Search for ScrollViewer, breadth-first
-	private static ScrollViewer FindScrollViewer(DependencyObject root)
-	{
-		var queue = new Queue<DependencyObject>(new[] { root });
-
-		do
-		{
-			var item = queue.Dequeue();
-
-			if (item is ScrollViewer)
-				return (ScrollViewer)item;
-
-			for (var i = 0; i < VisualTreeHelper.GetChildrenCount(item); i++)
-				queue.Enqueue(VisualTreeHelper.GetChild(item, i));
-		} while (queue.Count > 0);
-
-		return null;
 	}
 }
