283b3c96cbcc6c6c8ebcda3b7f5efa962adb3877
gre4bee <gre4bee@gmail.com>
2022-07-13 15:50:28 +0800

    Prevent window from moving if layout is locked

--- ElectronicObserver/FormMainWpf.xaml.cs
+++ ElectronicObserver/FormMainWpf.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Interop;
+using ElectronicObserver.Utility;
 using ElectronicObserver.ViewModels;
 
 namespace ElectronicObserver;
@@ -25,5 +28,36 @@ public partial class FormMainWpf : System.Windows.Window
 
 		Loaded += (sender, _) => ViewModel.LoadLayout(sender);
 		Closed += (sender, _) => ViewModel.SaveLayout(sender);
+		SourceInitialized += FormMainWpf_SourceInitialized;
+	}
+
+	private void FormMainWpf_SourceInitialized(object? sender, System.EventArgs e)
+	{
+		WindowInteropHelper helper = new WindowInteropHelper(this);
+		HwndSource source = HwndSource.FromHwnd(helper.Handle);
+		source.AddHook(WndProc);
+	}
+
+	const int WM_SYSCOMMAND = 0x0112;
+	const int SC_MOVE = 0xF010;
+
+	private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+	{
+
+		switch (msg)
+		{
+			case WM_SYSCOMMAND:
+				int command = wParam.ToInt32() & 0xfff0;
+				if (command == SC_MOVE)
+				{
+					var c = Configuration.Config;
+					if (c.Life.LockLayout)
+						handled = true;
+				}
+				break;
+			default:
+				break;
+		}
+		return IntPtr.Zero;
 	}
 }
