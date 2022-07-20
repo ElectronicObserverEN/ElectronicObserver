7bc33ef61cea8fbe2713c1c434b12e0c6e1c12f8
Jebzou <22751386+Jebzou@users.noreply.github.com>
2022-07-20 21:47:53 +0200

    Load Fit bonuses

---
+++ ElectronicObserverTypes/Serialization/FitBonus/FitBonusData.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.Json.Serialization;
+using System.Collections.Generic;
+
+namespace ElectronicObserverTypes.Serialization.FitBonus
+{
+    public class FitBonusData
+    {
+        [JsonPropertyName("shipClass")] public List<int>? ShipClasses { get; set; }
+
+        /// <summary>
+        /// Master id = exact id of the ship
+        /// </summary>
+        [JsonPropertyName("shipX")] public List<int>? ShipMasterIds { get; set; }
+
+        /// <summary>
+        /// Base id of the ship (minimum remodel), bonus applies to all of the ship forms
+        /// </summary>
+        [JsonPropertyName("shipS")] public List<int>? ShipIds { get; set; }
+
+        [JsonPropertyName("shipType")] public List<int>? ShipTypes { get; set; }
+
+
+        [JsonPropertyName("requires")] public List<int>? EquipmentRequired { get; set; }
+
+        [JsonPropertyName("requiresNum")] public int? NumberOfEquipmentsRequired { get; set; }
+
+
+        [JsonPropertyName("requiresType")] public List<int>? EquipmentTypesRequired { get; set; }
+
+        [JsonPropertyName("requiresNumType")] public int? NumberOfEquipmentTypesRequired { get; set; }
+
+        /// <summary>
+        /// Improvment level of the equipment required
+        /// </summary>
+        [JsonPropertyName("level")] public int? EquipmentLevel { get; set; }
+
+        /// <summary>
+        /// Number Of Equipments Required after applying the improvment filter
+        /// </summary>
+        [JsonPropertyName("num")] public int? NumberOfEquipmentsRequiredAfterOtherFilters { get; set; }
+
+        /// <summary>
+        /// Bonuses to apply
+        /// Applied x times, x being the number of equipment matching the conditions of the bonus fit 
+        /// If NumberOfEquipmentsRequiredAfterOtherFilters or EquipmentRequired or EquipmentTypesRequired, bonus is applied only once
+        /// </summary>
+        [JsonPropertyName("bonus")] public FitBonusValue Bonuses { get; set; } = null;
+
+        /// <summary>
+        /// Bonuses to apply if ship had a radar with LOS >= 5
+        /// </summary>
+        [JsonPropertyName("bonusSR")] public FitBonusValue BonusesIfLOSRadar { get; set; } = null;
+
+        /// <summary>
+        /// Bonuses to apply if ship had a radar with AA >= 2
+        /// </summary>
+        [JsonPropertyName("bonusAR")] public FitBonusValue BonusesIfAirRadar { get; set; } = null;
+
+    }
+}
---
+++ ElectronicObserverTypes/Serialization/FitBonus/FitBonusPerEquipment.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace ElectronicObserverTypes.Serialization.FitBonus
+{
+    public class FitBonusPerEquipment
+    {
+        [JsonPropertyName("types")] public List<int>? EquipmentTypes { get; set; }
+
+        [JsonPropertyName("ids")] public List<int>? EquipmentIds { get; set; }
+
+        [JsonPropertyName("bonuses")] public List<FitBonusData>? Bonuses { get; set; }
+    }
+}
