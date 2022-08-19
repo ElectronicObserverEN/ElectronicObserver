a894710088e0d94be4b24bc6fcda757b24d120c6
gre4bee <gre4bee@gmail.com>
2022-08-19 17:46:11 +0800

    Multi-target dotnet7 and publish profile for dotnet7

--- Browser/AirControlSimulator/AirControlSimulator.Designer.cs
+++ Browser/AirControlSimulator/AirControlSimulator.Designer.cs
@@ -39,7 +39,7 @@ namespace Browser.AirControlSimulator {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Browser.AirControlSimulator.AirControlSimulator", typeof(AirControlSimulator).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AirControlSimulator.AirControlSimulator", typeof(AirControlSimulator).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
