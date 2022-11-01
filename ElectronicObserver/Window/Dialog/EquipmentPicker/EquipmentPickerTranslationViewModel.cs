76ea895341a285992d21dab67e236c9af8607b02
Jebzou <22751386+Jebzou@users.noreply.github.com>
2022-11-01 11:23:23 +0100

    Equipment picker

--- ElectronicObserver/Window/Dialog/EquipmentPicker/EquipmentPickerTranslationViewModel.cs
+++ ElectronicObserver/Window/Dialog/EquipmentPicker/EquipmentPickerTranslationViewModel.cs
@@ -6,5 +6,7 @@ public class EquipmentPickerTranslationViewModel : TranslationBaseViewModel
 {
 	public string Level => Data.ShipGroup.ExpressionDataRes.Level;
 
+	public string Name => EncycloRes.EquipName;
+
 	public string AircraftLevel => EncycloRes.SkillLevel;
 }
