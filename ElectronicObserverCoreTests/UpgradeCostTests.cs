34fb93031c6d9e55d54590dd4ebe83d09143250a
Jebzou <22751386+Jebzou@users.noreply.github.com>
2022-12-03 13:14:07 +0100

    More unit test

--- ElectronicObserverCoreTests/UpgradeCostTests.cs
+++ ElectronicObserverCoreTests/UpgradeCostTests.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ElectronicObserverTypes.Mocks;
-using ElectronicObserverTypes.Serialization.FitBonus;
-using ElectronicObserverTypes;
-using Xunit;
+﻿using ElectronicObserver.Utility.Data;
 using ElectronicObserver.Window.Tools.EquipmentUpgradePlanner;
-using ElectronicObserver.Utility.Data;
-using Microsoft.Extensions.Hosting;
+using ElectronicObserverTypes;
+using ElectronicObserverTypes.Mocks;
+using Xunit;
 
 namespace ElectronicObserverCoreTests;
 
@@ -108,9 +101,9 @@ public class UpgradeCostTests
 			Steel = 11 * 750,
 			Bauxite = 11 * 100,
 
-			DevelopmentMaterial = 
+			DevelopmentMaterial =
 			// 0 -> 6
-			(6 * 10) + 
+			(6 * 10) +
 			// 6 -> 7
 			(1 * 16) +
 			// 7 -> Max (Slider)
@@ -154,4 +147,168 @@ public class UpgradeCostTests
 
 		Assert.Equal(expectedCost, cost);
 	}
+
+
+	[Fact]
+	public void UpgradeCostTest3()
+	{
+		Assert.NotEmpty(UpgradeData.UpgradeList);
+
+		EquipmentUpgradePlanItemModel plan = new()
+		{
+			// lvl 8 -> Conversion
+			DesiredUpgradeLevel = UpgradeLevel.Conversion,
+			EquipmentId = EquipmentId.MainGunSmall_5inchSingleGunMk_30Kai,
+			SliderLevel = SliderUpgradeLevel.Eight,
+			SelectedHelper = ShipId.FletcherMkII
+		};
+
+		EquipmentDataMock equipment = new EquipmentDataMock(Db.MasterEquipment[plan.EquipmentId])
+		{
+			UpgradeLevel = UpgradeLevel.Eight
+		};
+		IShipDataMaster helper = Db.MasterShips[plan.SelectedHelper];
+
+		EquipmentUpgradePlanCostModel cost = equipment.CalculateUpgradeCost(UpgradeData.UpgradeList, helper, plan.DesiredUpgradeLevel, plan.SliderLevel);
+
+
+		EquipmentUpgradePlanCostModel expectedCost = new EquipmentUpgradePlanCostModel()
+		{
+			Fuel = 3 * 30,
+			Ammo = 3 * 90,
+			Steel = 3 * 190,
+			Bauxite = 3 * 180,
+
+			DevelopmentMaterial =
+			// 8 -> Max (Slider)
+			(2 * 22) +
+			// Conversion (Slider)
+			(1 * 39),
+
+			ImprovmentMaterial =
+			// 7 -> Max (Slider)
+			(2 * 11) +
+			// Conversion (Slider)
+			(1 * 18),
+
+			RequiredConsumables = new()
+			{
+				new EquipmentUpgradePlanCostItemModel()
+				{
+					Id = (int)UseItemId.NewModelArmamentMaterials,
+					Required = 2
+				},
+				new EquipmentUpgradePlanCostItemModel()
+				{
+					Id = (int)UseItemId.Medals,
+					Required = 2
+				},
+			},
+			RequiredEquipments = new()
+			{
+				// 8 -> Max
+				new EquipmentUpgradePlanCostItemModel()
+				{
+					Id = (int)EquipmentId.MainGunSmall_5inchSingleGunMk_30,
+					Required = 2 * 1
+				},
+				// Conversion
+				new EquipmentUpgradePlanCostItemModel()
+				{
+					Id = (int)EquipmentId.RadarSmall_GFCSMk_37,
+					Required = 1
+				}
+			}
+		};
+
+		Assert.Equal(expectedCost, cost);
+	}
+
+	[Fact]
+	public void UpgradeCostTest4()
+	{
+		Assert.NotEmpty(UpgradeData.UpgradeList);
+
+		EquipmentUpgradePlanItemModel plan = new()
+		{
+			// lvl 0 -> Conversion
+			DesiredUpgradeLevel = UpgradeLevel.Conversion,
+			EquipmentId = EquipmentId.CarrierBasedBomber_Type99DiveBomber_EgusaSquadron,
+			SliderLevel = SliderUpgradeLevel.Conversion,
+			SelectedHelper = ShipId.SouryuuKaiNi
+		};
+
+		EquipmentDataMock equipment = new EquipmentDataMock(Db.MasterEquipment[plan.EquipmentId])
+		{
+			UpgradeLevel = UpgradeLevel.Zero
+		};
+		IShipDataMaster helper = Db.MasterShips[plan.SelectedHelper];
+
+		EquipmentUpgradePlanCostModel cost = equipment.CalculateUpgradeCost(UpgradeData.UpgradeList, helper, plan.DesiredUpgradeLevel, plan.SliderLevel);
+
+
+		EquipmentUpgradePlanCostModel expectedCost = new EquipmentUpgradePlanCostModel()
+		{
+			Fuel = 11 * 300,
+			Ammo = 11 * 300,
+			Steel = 11 * 0,
+			Bauxite = 11 * 390,
+
+			DevelopmentMaterial =
+			// 0 -> 6
+			(6 * 6) +
+			// 6 -> Max
+			(4 * 7) +
+			// Conversion (Slider)
+			(1 * 28),
+
+			ImprovmentMaterial =
+			// 0 -> 6
+			(6 * 4) +
+			// 6 -> Max
+			(4 * 5) +
+			// Conversion (Slider)
+			(1 * 20),
+
+			RequiredConsumables = new()
+			{
+				new EquipmentUpgradePlanCostItemModel()
+				{
+					Id = (int)UseItemId.NewModelAviationArmamentMaterials,
+					Required = 3
+				},
+				new EquipmentUpgradePlanCostItemModel()
+				{
+					Id = (int)UseItemId.SkilledCrewMember,
+					Required = 2
+				},
+			},
+			RequiredEquipments = new()
+			{
+				// 0 -> 6
+				new EquipmentUpgradePlanCostItemModel()
+				{
+					Id = (int)EquipmentId.CarrierBasedBomber_Type99DiveBomber,
+					Required = 6 * 2
+				},
+				// 6 -> Max
+				new EquipmentUpgradePlanCostItemModel()
+				{
+					Id = (int)EquipmentId.CarrierBasedBomber_Type99DiveBomberModel22,
+					Required = 4 * 1
+				},
+				// Conversion (Slider)
+				new EquipmentUpgradePlanCostItemModel()
+				{
+					Id = (int)EquipmentId.CarrierBasedBomber_Suisei,
+					Required = 1 * 6
+				},
+			}
+		};
+
+		Assert.Equal(expectedCost, cost);
+
+
+	}
+
 }
