a166c4d7d2c013b63cf6fe96321f7ab8b3ded5db
Jebzou <22751386+Jebzou@users.noreply.github.com>
2022-12-16 07:22:49 +0100

    Time change service

---
+++ ElectronicObserver/Services/TimeChangeService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using ElectronicObserver.Utility;
+using ElectronicObserver.Utility.Mathematics;
+
+namespace ElectronicObserver.Services;
+
+/// <summary>
+/// Service that triggers events on time change (day changed, ...)
+/// </summary>
+public class TimeChangeService : ObservableObject
+{
+	public DayOfWeek TodayJST { get; private set; }
+
+	public event Action TodayChanged = delegate { };
+
+	public TimeChangeService()
+	{
+		PropertyChanged += TimeChangeService_PropertyChanged;
+
+		SystemEvents.UpdateTimerTick += SystemEvents_UpdateTimerTick;
+	}
+
+	private void SystemEvents_UpdateTimerTick()
+	{
+		TodayJST = DateTimeHelper.GetJapanStandardTimeNow().DayOfWeek;
+	}
+
+	private void TimeChangeService_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+	{
+		if (e.PropertyName == nameof(TodayJST)) TodayChanged();
+	}
+}
