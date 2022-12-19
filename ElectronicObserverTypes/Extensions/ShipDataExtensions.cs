4695ae768ce82fd72ad89b926d1e1f75bc42f51a
gre4bee <gre4bee@gmail.com>
2022-12-19 16:59:32 +0800

    Rename Methods for Readability

--- ElectronicObserverTypes/Extensions/ShipDataExtensions.cs
+++ ElectronicObserverTypes/Extensions/ShipDataExtensions.cs
@@ -473,8 +473,8 @@ public static class ShipDataExtensions
 
 		{ MasterShip.ShipType: ShipTypes.LightAircraftCarrier } => ship.ASWTotal switch
 		{
-			>= 100 => ship.OneHundredASWCond() || ship.SixtyFiveASWCond() || ship.ZeroSlotASWCond(),
-			>= 65 => ship.SixtyFiveASWCond() || ship.ZeroSlotASWCond(),
+			>= 100 => ship.AswCondition100() || ship.AswCondition65() || ship.ZeroSlotASWCond(),
+			>= 65 => ship.AswCondition65() || ship.ZeroSlotASWCond(),
 			>= 50 => ship.MasterShip.ShipId switch
 			{
 				ShipId.SuzuyaCVLKaiNi or ShipId.KumanoCVLKaiNi => false,
@@ -507,8 +507,8 @@ public static class ShipDataExtensions
 		_ => false,
 	};
 	public static bool ZeroSlotASWCond(this IShipData ship) => ship.HasAswPatrolAircraft(size: -1) && ship.HasAntiSubmarineAircraft();
-	public static bool SixtyFiveASWCond(this IShipData ship) => ship.HasSpecialAntiSubmarineAttacker() || ship.HasAswPatrolAircraft();
-	public static bool OneHundredASWCond(this IShipData ship) => ship.HasSonar() && ship.HasAntiSubmarineAircraft();
+	public static bool AswCondition65(this IShipData ship) => ship.HasSpecialAntiSubmarineAttacker() || ship.HasAswPatrolAircraft();
+	public static bool AswCondition100(this IShipData ship) => ship.HasSonar() && ship.HasAntiSubmarineAircraft();
 	private static bool HyuugaK2OpeningAswCondition(this IShipData ship)
 	{
 		List<IEquipmentData?> eqs = ship.AllSlotInstance
