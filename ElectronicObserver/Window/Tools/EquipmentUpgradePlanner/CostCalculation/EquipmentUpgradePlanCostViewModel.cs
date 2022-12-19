da7facdacca63fffa1969c553cc261823666d621
Jebzou <22751386+Jebzou@users.noreply.github.com>
2022-12-19 22:47:26 +0100

    Cost display

--- ElectronicObserver/Window/Tools/EquipmentUpgradePlanner/CostCalculation/EquipmentUpgradePlanCostViewModel.cs
+++ ElectronicObserver/Window/Tools/EquipmentUpgradePlanner/CostCalculation/EquipmentUpgradePlanCostViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using CommunityToolkit.Mvvm.ComponentModel;
 using ElectronicObserverTypes;
 
 namespace ElectronicObserver.Window.Tools.EquipmentUpgradePlanner.CostCalculation;
 
-public class EquipmentUpgradePlanCostViewModel
+public class EquipmentUpgradePlanCostViewModel : ObservableObject
 {
 	public EquipmentUpgradePlanCostModel Model { get; set; }
 
@@ -30,6 +32,8 @@ public class EquipmentUpgradePlanCostViewModel
 
 	public List<EquipmentUpgradePlanCostConsumableViewModel> RequiredConsumables { get; set; } = new();
 
+	public bool HasEnoughResources => GetHasEnoughResourceValue();
+
 	public EquipmentUpgradePlanCostViewModel(EquipmentUpgradePlanCostModel model)
 	{
 		Model = model;
@@ -44,6 +48,53 @@ public class EquipmentUpgradePlanCostViewModel
 
 		RequiredEquipments = model.RequiredEquipments.Select(item => new EquipmentUpgradePlanCostEquipmentViewModel(item)).ToList();
 		RequiredConsumables = model.RequiredConsumables.Select(item => new EquipmentUpgradePlanCostConsumableViewModel(item)).ToList();
+
+		Fuel.PropertyChanged += ResourcePropertyUpdated;
+		Ammo.PropertyChanged += ResourcePropertyUpdated;
+		Steel.PropertyChanged += ResourcePropertyUpdated;
+		Bauxite.PropertyChanged += ResourcePropertyUpdated;
+
+		DevelopmentMaterial.PropertyChanged += ResourcePropertyUpdated;
+		ImprovementMaterial.PropertyChanged += ResourcePropertyUpdated;
+
+		foreach (EquipmentUpgradePlanCostEquipmentViewModel equip in RequiredEquipments)
+		{
+			equip.PropertyChanged += ResourcePropertyUpdated;
+		}
+
+		foreach (EquipmentUpgradePlanCostConsumableViewModel consumable in RequiredConsumables)
+		{
+			consumable.PropertyChanged += ResourcePropertyUpdated;
+		}
+
+		Update();
 	}
 
+	private void ResourcePropertyUpdated(object? sender, PropertyChangedEventArgs e)
+	{
+		if (e.PropertyName is not nameof(EquipmentUpgradePlanResourceDisplayViewModel.Owned) and not nameof(EquipmentUpgradePlanResourceDisplayViewModel.Required)) return;
+
+		Update();
+	}
+
+	private void Update()
+	{
+		OnPropertyChanged(nameof(HasEnoughResources));
+	}
+
+	private bool GetHasEnoughResourceValue()
+	{
+		if (!Fuel.EnoughOwned) return false;
+		if (!Ammo.EnoughOwned) return false;
+		if (!Steel.EnoughOwned) return false;
+		if (!Bauxite.EnoughOwned) return false;
+
+		if (!DevelopmentMaterial.EnoughOwned) return false;
+		if (!ImprovementMaterial.EnoughOwned) return false;
+
+		if (RequiredEquipments.Any(equip => !equip.EnoughOwned)) return false;
+		if (RequiredConsumables.Any(consumable => !consumable.EnoughOwned)) return false;
+
+		return true;
+	}
 }
