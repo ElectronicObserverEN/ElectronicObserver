fd3eee8d15294080f1b94aebf9983a2dbb19f56b
Jebzou <22751386+Jebzou@users.noreply.github.com>
2023-01-04 22:21:12 +0100

    Ship picker copy pasta

---
+++ ElectronicObserver/Window/Dialog/ShipDataPicker/ShipDataPickerView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using ElectronicObserverTypes;
+
+namespace ElectronicObserver.Window.Dialog.ShipDataPicker;
+/// <summary>
+/// Interaction logic for ShipPickerView.xaml
+/// </summary>
+public partial class ShipDataPickerView
+{
+	public ShipDataPickerView(ShipDataPickerViewModel viewModel) : base(viewModel)
+	{
+		InitializeComponent();
+
+		ViewModel.SelectShipCommand.Execute(null);
+
+		ViewModel.PropertyChanged += ViewModel_PropertyChanged;
+
+		Closing += ShipPicker_Closing;
+	}
+
+	private void ShipPicker_Closing(object? sender, System.ComponentModel.CancelEventArgs e)
+	{
+		ViewModel.PropertyChanged -= ViewModel_PropertyChanged;
+	}
+
+	private void ViewModel_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+	{
+		if (e.PropertyName is not nameof(ViewModel.PickedShip)) return;
+
+		PickedShip = ViewModel.PickedShip;
+		DialogResult = true;
+	}
+
+
+	public IShipData? PickedShip { get; private set; }
+}
---
+++ ElectronicObserver/Window/Dialog/ShipDataPicker/ShipDataPickerViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using CommunityToolkit.Mvvm.Input;
+using ElectronicObserver.Common;
+using ElectronicObserver.Data;
+using ElectronicObserver.Window.Tools.EventLockPlanner;
+using ElectronicObserverTypes;
+
+namespace ElectronicObserver.Window.Dialog.ShipDataPicker;
+
+public partial class ShipDataPickerViewModel : WindowViewModelBase
+{
+	private List<IShipData> AllShips { get; }
+
+	public ObservableCollection<IShipData> ShipsFiltered { get; set; } = new();
+
+	public ShipFilterViewModel Filters { get; } = new();
+
+	public IShipData? PickedShip { get; private set; }
+
+	public ShipDataPickerTranslationViewModel ShipDataPicker { get; set; } = new();
+
+	public ShipDataPickerViewModel()
+	{
+		AllShips = KCDatabase.Instance.Ships
+			.Values
+			.Cast<IShipData>()
+			.ToList();
+
+		Filters.PropertyChanged += ReloadShips;
+	}
+
+	[RelayCommand]
+	private void SelectShip(IShipData? ship)
+	{
+		PickedShip = ship;
+	}
+
+	private void ReloadShips(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+	{
+		ShipsFiltered.Clear();
+
+		List<IShipData> filteredShips = AllShips.Where(Filters.MeetsFilterCondition).ToList();
+
+		foreach (IShipData ship in filteredShips)
+		{
+			ShipsFiltered.Add(ship);
+		}
+	}
+}
