de21d8c11dd4dd463de7487e2209f468155ba7b4
Jebzou <22751386+Jebzou@users.noreply.github.com>
2023-01-31 20:01:48 +0100

    Pager design

---
+++ ElectronicObserver/Window/Tools/EquipmentUpgradePlanner/Paging/PagingControlTranslationViewModel.cs
@@ -0,0 +1,9 @@
+﻿using ElectronicObserver.ViewModels.Translations;
+
+namespace ElectronicObserver.Window.Tools.EquipmentUpgradePlanner.Paging;
+
+public class PagingControlTranslationViewModel : TranslationBaseViewModel
+{
+	public string ItemsPerPage => PagingControl.ItemsPerPage;
+	public string Page => PagingControl.Page;
+}
--- ElectronicObserver/Window/Tools/EquipmentUpgradePlanner/Paging/PagingControlViewModel.cs
+++ ElectronicObserver/Window/Tools/EquipmentUpgradePlanner/Paging/PagingControlViewModel.cs
@@ -19,13 +19,21 @@ public partial class PagingControlViewModel : ObservableObject
 
 	public int LastPage => (int)Math.Ceiling(Items.Count / (decimal)ItemsPerPage);
 
-	public bool NextPageEnabled => CurrentPage < LastPage;
-	public bool PreviousPageEnabled => CurrentPage > 1;
+	public PagingControlTranslationViewModel PagingControl { get; set; } = new();
 
 	public PagingControlViewModel()
 	{
 		PropertyChanged += OnPagerUpdate;
+		PropertyChanged += OnPagerUpdate2;
 		UpdateCollection();
+	}
+
+	private void OnPagerUpdate2(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+	{
+		if (e.PropertyName is nameof(CurrentPage) or nameof(LastPage)) return;
+
+		NextPageCommand.NotifyCanExecuteChanged();
+		PreviousPageCommand.NotifyCanExecuteChanged();
 	}
 
 	public PagingControlViewModel(ObservableCollection<object?> items) : this()
@@ -43,22 +51,21 @@ public partial class PagingControlViewModel : ObservableObject
 	{
 		if (e.PropertyName is not nameof(CurrentPage) and not nameof(ItemsPerPage)) return;
 
-		OnPropertyChanged(nameof(PreviousPageEnabled));
-		OnPropertyChanged(nameof(NextPageEnabled));
-
 		UpdateCollection();
 	}
 
 	private void UpdateCollection()
 	{
 		// dont go out of bound
-		CurrentPage = Math.Max(CurrentPage, 0);
+		CurrentPage = Math.Max(CurrentPage, Items.Any() ? 1 : 0);
 		CurrentPage = Math.Min(CurrentPage, LastPage);
 
 		ItemsPaged = Items
 			.Skip(ItemsPerPage * (CurrentPage - 1))
 			.Take(ItemsPerPage)
 			.ToList();
+
+		OnPropertyChanged(nameof(LastPage));
 	}
 
 	/// <summary>
@@ -81,13 +88,16 @@ public partial class PagingControlViewModel : ObservableObject
 		UpdateCollection();
 	}
 
-	[RelayCommand]
+	public bool NextPageEnabled() => CurrentPage < LastPage;
+	public bool PreviousPageEnabled() => CurrentPage > 1;
+
+	[RelayCommand(CanExecute = nameof(NextPageEnabled))]
 	public void NextPage()
 	{
 		CurrentPage++;
 	}
 
-	[RelayCommand]
+	[RelayCommand(CanExecute = nameof(PreviousPageEnabled))]
 	public void PreviousPage()
 	{
 		CurrentPage--;
