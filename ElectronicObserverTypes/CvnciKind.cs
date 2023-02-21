34bdc6dd0cc1cac2b1a9639b1c4a9780fdb203a0
MyAngelKamikaze <myangelkamikaze@gmail.com>
2023-02-21 17:06:55 +0900

    initial refactoring

--- ElectronicObserverTypes/CvnciKind.cs
+++ ElectronicObserverTypes/CvnciKind.cs
@@ -5,10 +5,13 @@ public enum CvnciKind
 	Unknown,
 
 	FighterFighterAttacker,
+
 	FighterAttacker,
+
 	/// <summary>
 	/// 彗星一二型(三一号光電管爆弾搭載機)
 	/// </summary>
 	Phototube,
-	FighterOtherOther
+
+	FighterOtherOther,
 }
