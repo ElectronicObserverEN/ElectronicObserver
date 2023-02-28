c1ae640afa3d58dce3c802ff97a854223a019823
gre4bee <gre4bee@gmail.com>
2023-02-28 18:03:08 +0800

    Adjust bypass for webview2

--- Browser/WebView2Browser/WebView2ViewModel.cs
+++ Browser/WebView2Browser/WebView2ViewModel.cs
@@ -372,7 +372,7 @@ public class WebView2ViewModel : BrowserViewModel
 	{
 		if (e.Request.Uri.Contains(@"gadget_html5") && Configuration.UseGadgetRedirect)
 		{
-			e.Request.Uri = e.Request.Uri.Replace("http://203.104.209.7/gadget_html5/", "https://kcwiki.github.io/cache/gadget_html5/");
+			e.Request.Uri = e.Request.Uri.Replace("http://203.104.209.7/gadget_html5/", "https://electronicobserveren.github.io/cache/gadget_html5/");
 		}
 		if (e.Request.Uri.Contains("/kcs2/resources/bgm/"))
 		{
