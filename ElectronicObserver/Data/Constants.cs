7292ff20c32e2097e2284b755f5fe672d172107c
MyAngelKamikaze <myangelkamikaze@gmail.com>
2023-03-19 22:28:42 +0900

    extract day attack

--- ElectronicObserver/Data/Constants.cs
+++ ElectronicObserver/Data/Constants.cs
@@ -821,45 +821,6 @@ public static class Constants
 	}
 
 
-
-	/// <summary>
-	/// 昼戦攻撃種別を表す文字列を取得します。
-	/// </summary>
-	public static string GetDayAttackKind(DayAttackKind id) => id switch
-	{
-		DayAttackKind.NormalAttack => AttackResources.NormalAttack,
-		DayAttackKind.Laser => AttackResources.LaserAttack,
-		DayAttackKind.DoubleShelling => AttackResources.DoubleShelling,
-		DayAttackKind.CutinMainSub => AttackResources.CutinMainSub,
-		DayAttackKind.CutinMainRadar => AttackResources.CutinMainRadar,
-		DayAttackKind.CutinMainAP => AttackResources.CutinMainAp,
-		DayAttackKind.CutinMainMain => AttackResources.CutinMainMain,
-		DayAttackKind.CutinAirAttack => AttackResources.CutinAirAttack,
-		DayAttackKind.SpecialNelson => AttackResources.SpecialNelson,
-		DayAttackKind.SpecialNagato => AttackResources.SpecialNagato,
-		DayAttackKind.SpecialMutsu => AttackResources.SpecialMutsu,
-		DayAttackKind.SpecialColorado => AttackResources.SpecialColorado,
-		DayAttackKind.SpecialKongo => AttackResources.SpecialKongou,
-		DayAttackKind.ZuiunMultiAngle => AttackResources.ZuiunMultiAngle,
-		DayAttackKind.SeaAirMultiAngle => AttackResources.SeaAirMultiAngle,
-		DayAttackKind.SpecialSubmarineTender23 => AttackResources.SpecialSubmarineTender23,
-		DayAttackKind.SpecialSubmarineTender34 => AttackResources.SpecialSubmarineTender34,
-		DayAttackKind.SpecialSubmarineTender24 => AttackResources.SpecialSubmarineTender24,
-		DayAttackKind.SpecialYamato3Ships => AttackResources.SpecialYamato123,
-		DayAttackKind.SpecialYamato2Ships => AttackResources.SpecialYamato12,
-		DayAttackKind.Shelling => AttackResources.Shelling,
-		DayAttackKind.AirAttack => AttackResources.AirAttack,
-		DayAttackKind.DepthCharge => AttackResources.DepthChargeAttack,
-		DayAttackKind.Torpedo => AttackResources.TorpedoAttack,
-		DayAttackKind.Rocket => AttackResources.RocketAttack,
-		DayAttackKind.LandingDaihatsu => AttackResources.LandingDaihatsu,
-		DayAttackKind.LandingTokuDaihatsu => AttackResources.LandingTokuDaihatsu,
-		DayAttackKind.LandingDaihatsuTank => AttackResources.LandingDaihatsuTank,
-		DayAttackKind.LandingAmphibious => AttackResources.TankAttack,
-		DayAttackKind.LandingTokuDaihatsuTank => AttackResources.LandingTokuDaihatsuTank,
-		_ => $"{AttackResources.Unknown}({(int)id})",
-	};
-
 	/// <summary>
 	/// 勝利ランクを表すIDを取得します。
 	/// </summary>
