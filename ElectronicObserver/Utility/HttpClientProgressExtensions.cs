d075f6481afc351245b24d0e7e601ce3e032ae29
Jebzou <22751386+Jebzou@users.noreply.github.com>
2023-04-04 05:48:50 +0200

    Sonar

--- ElectronicObserver/Utility/HttpClientProgressExtensions.cs
+++ ElectronicObserver/Utility/HttpClientProgressExtensions.cs
@@ -11,29 +11,29 @@ namespace ElectronicObserver.Utility
 	/// </summary>
 	public static class HttpClientProgressExtensions
 	{
-		public static async Task DownloadDataAsync(this HttpClient client, string requestUrl, Stream destination, IProgress<float> progress = null, CancellationToken cancellationToken = default(CancellationToken))
+		public static async Task DownloadDataAsync(this HttpClient client, string requestUrl, Stream destination, IProgress<float> progress)
+			=> await client.DownloadDataAsync(requestUrl, destination, progress, default);
+
+		public static async Task DownloadDataAsync(this HttpClient client, string requestUrl, Stream destination, IProgress<float> progress, CancellationToken cancellationToken)
 		{
-			using (var response = await client.GetAsync(requestUrl, HttpCompletionOption.ResponseHeadersRead))
+			using var response = await client.GetAsync(requestUrl, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
+
+			var contentLength = response.Content.Headers.ContentLength;
+			using var download = await response.Content.ReadAsStreamAsync(cancellationToken);
+
+			if (progress is null || !contentLength.HasValue)
 			{
-				var contentLength = response.Content.Headers.ContentLength;
-				using (var download = await response.Content.ReadAsStreamAsync())
-				{
-					// no progress... no contentLength... very sad
-					if (progress is null || !contentLength.HasValue)
-					{
-						await download.CopyToAsync(destination);
-						return;
-					}
-					// Such progress and contentLength much reporting Wow!
-					var progressWrapper = new Progress<long>(totalBytes => progress.Report(GetProgressPercentage(totalBytes, contentLength.Value)));
-					await download.CopyToAsync(destination, 81920, progressWrapper, cancellationToken);
-				}
+				await download.CopyToAsync(destination, cancellationToken);
+				return;
 			}
 
-			float GetProgressPercentage(float totalBytes, float currentBytes) => (totalBytes / currentBytes) * 100f;
+			var progressWrapper = new Progress<long>(totalBytes => progress.Report(GetProgressPercentage(totalBytes, contentLength.Value)));
+			await download.CopyToAsync(destination, 81920, progressWrapper, cancellationToken);
+
+			static float GetProgressPercentage(float totalBytes, float currentBytes) => (totalBytes / currentBytes) * 100f;
 		}
 
-		static async Task CopyToAsync(this Stream source, Stream destination, int bufferSize, IProgress<long> progress = null, CancellationToken cancellationToken = default(CancellationToken))
+		static async Task CopyToAsync(this Stream source, Stream destination, int bufferSize, IProgress<long> progress, CancellationToken cancellationToken)
 		{
 			if (bufferSize < 0)
 				throw new ArgumentOutOfRangeException(nameof(bufferSize));
@@ -49,9 +49,9 @@ namespace ElectronicObserver.Utility
 			var buffer = new byte[bufferSize];
 			long totalBytesRead = 0;
 			int bytesRead;
-			while ((bytesRead = await source.ReadAsync(buffer, 0, buffer.Length, cancellationToken).ConfigureAwait(false)) != 0)
+			while ((bytesRead = await source.ReadAsync(buffer, cancellationToken).ConfigureAwait(false)) != 0)
 			{
-				await destination.WriteAsync(buffer, 0, bytesRead, cancellationToken).ConfigureAwait(false);
+				await destination.WriteAsync(buffer.AsMemory(0, bytesRead), cancellationToken).ConfigureAwait(false);
 				totalBytesRead += bytesRead;
 				progress?.Report(totalBytesRead);
 			}
