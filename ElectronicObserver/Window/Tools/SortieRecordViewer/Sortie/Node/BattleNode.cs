7936a7c4b3d261b10410922356b30301b0643a8d
MyAngelKamikaze <myangelkamikaze@gmail.com>
2023-04-23 16:19:08 +0900

    rework battle result display

--- ElectronicObserver/Window/Tools/SortieRecordViewer/Sortie/Node/BattleNode.cs
+++ ElectronicObserver/Window/Tools/SortieRecordViewer/Sortie/Node/BattleNode.cs
@@ -10,54 +10,16 @@ public class BattleNode : SortieNode
 {
 	public BattleData FirstBattle { get; }
 	public BattleData? SecondBattle { get; set; }
-	public BattleResult? BattleResult { get; private set; }
+	private BattleResult? BattleResult { get; set; }
 
 	public string Result => ConstantsRes.BattleDetail_Result;
 
-	public string? ResultRank => BattleResult switch
-	{
-		null => null,
-		_ => string.Format(ConstantsRes.BattleDetail_ResultRank, BattleResult.WinRank),
-	};
-
-	public string? ResultMvpMain => BattleResult?.MvpIndex switch
-	{
-		int i and >= 0 => FirstBattle.FleetsBeforeBattle.Fleet.MembersInstance[i] switch
-		{
-			IShipData ship => string.Format(ConstantsRes.BattleDetail_ResultMVPMain, ship.NameWithLevel),
-			_ => null,
-		},
-		_ => null,
-	};
-
-	public string? ResultMvpEscort => BattleResult?.MvpIndexCombined switch
-	{
-		int i and >= 0 => FirstBattle.FleetsBeforeBattle.EscortFleet?.MembersInstance[i] switch
-		{
-			IShipData ship => string.Format(ConstantsRes.BattleDetail_ResultMVPEscort, ship.NameWithLevel),
-			_ => null,
-		},
-		_ => null,
-	};
-
-	public string? AdmiralExp => BattleResult?.AdmiralExp switch
-	{
-		int exp => string.Format(ConstantsRes.BattleDetail_AdmiralExp, exp),
-		_ => null,
-	};
-
-	public string? BaseExp => BattleResult?.BaseExp switch
-	{
-		int exp => string.Format(ConstantsRes.BattleDetail_ShipExp, exp),
-		_ => null,
-	};
-
-	public string? DropShip => BattleResult switch
-	{
-		{ DroppedShipId: ShipId id } => string.Format(ConstantsRes.BattleDetail_Drop, KcDatabase.MasterShips[(int)id].NameEN),
-		null => null,
-		_ => ConstantsRes.BattleDetail_Drop + ConstantsRes.NoNode,
-	};
+	public string? ResultRank { get; private set; }
+	public string? ResultMvpMain { get; private set; }
+	public string? ResultMvpEscort { get; private set; }
+	public string? AdmiralExp { get; private set; }
+	public string? BaseExp { get; private set; }
+	public string? DropShip { get; private set; }
 
 	public BattleNode(IKCDatabase kcDatabase, int world, int map, int cell, BattleData battle)
 		: base(kcDatabase, world, map, cell)
@@ -68,5 +30,40 @@ public class BattleNode : SortieNode
 	public void AddResult(ApiReqSortieBattleresultResponse result)
 	{
 		BattleResult = new(result);
+
+		ResultRank = string.Format(ConstantsRes.BattleDetail_ResultRank, BattleResult.WinRank);
+
+		ResultMvpMain = BattleResult.MvpIndex switch
+		{
+			int i and >= 0 => FirstBattle.FleetsBeforeBattle.Fleet.MembersInstance[i] switch
+			{
+				IShipData ship => string.Format(ConstantsRes.BattleDetail_ResultMVPMain, ship.NameWithLevel),
+				_ => null,
+			},
+			_ => null,
+		};
+
+		ResultMvpEscort = BattleResult.MvpIndexCombined switch
+		{
+			int i and >= 0 => FirstBattle.FleetsBeforeBattle.EscortFleet?.MembersInstance[i] switch
+			{
+				IShipData ship => string.Format(ConstantsRes.BattleDetail_ResultMVPEscort, ship.NameWithLevel),
+				_ => null,
+			},
+			_ => null,
+		};
+
+		AdmiralExp = string.Format(ConstantsRes.BattleDetail_AdmiralExp, BattleResult.AdmiralExp);
+		BaseExp = string.Format(ConstantsRes.BattleDetail_ShipExp, BattleResult.BaseExp);
+
+		DropShip = BattleResult.DroppedShipId switch
+		{
+			ShipId id => KcDatabase.MasterShips[(int)id] switch
+			{
+				{ } ship => $"{ConstantsRes.BattleDetail_Drop} {ship.ShipTypeName} {ship.NameWithClass}",
+				_ => ConstantsRes.BattleDetail_Drop + ConstantsRes.NoNode,
+			},
+			_ => null,
+		};
 	}
 }
