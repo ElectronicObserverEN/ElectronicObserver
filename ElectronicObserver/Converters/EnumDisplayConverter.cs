65aa98ad1cf0bdb3a7a9667f6c3adf2b4da5e4fb
Jebzou <22751386+Jebzou@users.noreply.github.com>
2023-05-27 11:06:19 +0200

    Translate equipment icon type

--- ElectronicObserver/Converters/EnumDisplayConverter.cs
+++ ElectronicObserver/Converters/EnumDisplayConverter.cs
@@ -14,6 +14,7 @@ public class EnumDisplayConverter : IValueConverter
 		{
 			EquipmentTypes e => KCDatabase.Instance.EquipmentTypes[(int)e].NameEN,
 			FormationType f => Constants.GetFormation(f),
+			EquipmentIconType eqIcon => eqIcon.TranslatedName(),
 			Enum e => e.Display(),
 			_ => "???"
 		};
