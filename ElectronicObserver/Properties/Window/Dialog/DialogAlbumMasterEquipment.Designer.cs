56cd3ff3ede0851c9a08ec6f1d3332168ecafaa5
Jebzou <22751386+Jebzou@users.noreply.github.com>
2023-06-09 19:47:57 +0200

    Upgrade cost display on equipments

--- ElectronicObserver/Properties/Window/Dialog/DialogAlbumMasterEquipment.Designer.cs
+++ ElectronicObserver/Properties/Window/Dialog/DialogAlbumMasterEquipment.Designer.cs
@@ -19,7 +19,7 @@ namespace ElectronicObserver.Properties.Window.Dialog {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class DialogAlbumMasterEquipment {
@@ -456,6 +456,15 @@ namespace ElectronicObserver.Properties.Window.Dialog {
         public static string TitleTorpedo {
             get {
                 return ResourceManager.GetString("TitleTorpedo", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 改修必要資材.
+        /// </summary>
+        public static string UpgradeCost {
+            get {
+                return ResourceManager.GetString("UpgradeCost", resourceCulture);
             }
         }
     }
---
+++ ElectronicObserver/Window/Tools/DialogAlbumMasterEquipment/EquipmentUpgrade/AlbumMasterEquipmentUpgradeViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElectronicObserver.Data;
+using ElectronicObserver.Data.Translation;
+using ElectronicObserverTypes;
+using ElectronicObserverTypes.Serialization.EquipmentUpgrade;
+
+namespace ElectronicObserver.Window.Tools.DialogAlbumMasterEquipment.EquipmentUpgrade;
+
+public class AlbumMasterEquipmentUpgradeViewModel
+{
+	public EquipmentUpgradeDataModel? UpgradeData { get; private set; }
+	private IEquipmentDataMaster Equipment { get; }
+
+	public EquipmentUpgradeImprovementCost EquipmentUpgradeCost { get; } = new();
+
+	public AlbumMasterEquipmentUpgradeViewModel(IEquipmentDataMaster equipment)
+	{
+		Equipment = equipment;
+		LoadUpgradeData();
+	}
+
+	private void LoadUpgradeData()
+	{
+		EquipmentUpgradeData upgradeData = KCDatabase.Instance.Translation.EquipmentUpgrade;
+		UpgradeData = upgradeData.UpgradeList.FirstOrDefault(upgrade => upgrade.EquipmentId == Equipment.ID);
+
+		EquipmentUpgradeImprovementModel? firstImprovement = UpgradeData?.Improvement.FirstOrDefault();
+
+		if (firstImprovement is null) return;
+
+		EquipmentUpgradeCost.Fuel = firstImprovement.Costs.Fuel;
+		EquipmentUpgradeCost.Ammo = firstImprovement.Costs.Ammo;
+		EquipmentUpgradeCost.Steel = firstImprovement.Costs.Steel;
+		EquipmentUpgradeCost.Bauxite = firstImprovement.Costs.Bauxite;
+
+		// ... 
+	}
+}
