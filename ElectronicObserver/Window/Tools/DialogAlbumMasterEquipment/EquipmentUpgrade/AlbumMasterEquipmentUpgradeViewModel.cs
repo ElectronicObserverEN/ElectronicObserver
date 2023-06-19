532bd2edfab77ec776ef3963e99e037d302347b6
Jebzou <22751386+Jebzou@users.noreply.github.com>
2023-06-19 16:26:16 +0200

    Helper display + removed code duplication

--- ElectronicObserver/Window/Tools/DialogAlbumMasterEquipment/EquipmentUpgrade/AlbumMasterEquipmentUpgradeViewModel.cs
+++ ElectronicObserver/Window/Tools/DialogAlbumMasterEquipment/EquipmentUpgrade/AlbumMasterEquipmentUpgradeViewModel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ElectronicObserver.Data;
 using ElectronicObserver.Data.Translation;
+using ElectronicObserver.Window.Tools.EquipmentUpgradePlanner.Helpers;
 using ElectronicObserverTypes;
 using ElectronicObserverTypes.Serialization.EquipmentUpgrade;
 
@@ -17,6 +18,8 @@ public class AlbumMasterEquipmentUpgradeViewModel
 
 	public EquipmentUpgradeImprovementCost EquipmentUpgradeCost { get; } = new();
 
+	public List<EquipmentUpgradeHelpersViewModel> Helpers { get; private set; } = new();
+
 	public AlbumMasterEquipmentUpgradeViewModel(IEquipmentDataMaster equipment)
 	{
 		Equipment = equipment;
@@ -28,7 +31,9 @@ public class AlbumMasterEquipmentUpgradeViewModel
 		EquipmentUpgradeData upgradeData = KCDatabase.Instance.Translation.EquipmentUpgrade;
 		UpgradeData = upgradeData.UpgradeList.FirstOrDefault(upgrade => upgrade.EquipmentId == Equipment.ID);
 
-		EquipmentUpgradeImprovementModel? firstImprovement = UpgradeData?.Improvement.FirstOrDefault();
+		if (UpgradeData is null) return;
+
+		EquipmentUpgradeImprovementModel? firstImprovement = UpgradeData.Improvement.FirstOrDefault();
 
 		if (firstImprovement is null) return;
 
@@ -36,6 +41,11 @@ public class AlbumMasterEquipmentUpgradeViewModel
 		EquipmentUpgradeCost.Ammo = firstImprovement.Costs.Ammo;
 		EquipmentUpgradeCost.Steel = firstImprovement.Costs.Steel;
 		EquipmentUpgradeCost.Bauxite = firstImprovement.Costs.Bauxite;
+
+		Helpers = UpgradeData.Improvement
+			.SelectMany(improvement => improvement.Helpers)
+			.Select(helperGroup => new EquipmentUpgradeHelpersViewModel(helperGroup))
+			.ToList();
 
 		// ... 
 	}
