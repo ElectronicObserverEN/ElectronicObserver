738a1e244749d5e91f2b10f376ea7c5e44456ee8
Jebzou <22751386+Jebzou@users.noreply.github.com>
2023-06-21 13:16:43 +0200

    SOnar

--- ElectronicObserver/Window/Tools/DialogAlbumMasterEquipment/EquipmentUpgrade/EquipmentUpgradeItemCostViewModel.cs
+++ ElectronicObserver/Window/Tools/DialogAlbumMasterEquipment/EquipmentUpgrade/EquipmentUpgradeItemCostViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using CommunityToolkit.Mvvm.DependencyInjection;
 using ElectronicObserver.Window.Tools.EquipmentUpgradePlanner.CostCalculation;
 using ElectronicObserverTypes.Serialization.EquipmentUpgrade;
 
