12a99fe1401be4370516ac477e3f6ebdf75a7106
Jebzou <22751386+Jebzou@users.noreply.github.com>
2023-06-23 13:11:09 +0200

    target type

--- ElectronicObserver/Data/Translation/DataAndTranslationManager.cs
+++ ElectronicObserver/Data/Translation/DataAndTranslationManager.cs
@@ -29,7 +29,7 @@ public class DataAndTranslationManager
 	{
 		Initialize();
 
-		DataAndTranslationIssueReporter = new DataAndTranslationIssueReporter();
+		DataAndTranslationIssueReporter = new();
 	}
 
 	public void Initialize()
