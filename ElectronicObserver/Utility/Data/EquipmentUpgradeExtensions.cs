643fb90c4f507f0fd03417c2bbee6e4662745429
Jebzou <22751386+Jebzou@users.noreply.github.com>
2023-10-25 18:15:38 +0200

    Equipment upgrade per ship aren't filtered by days

--- ElectronicObserver/Utility/Data/EquipmentUpgradeExtensions.cs
+++ ElectronicObserver/Utility/Data/EquipmentUpgradeExtensions.cs
@@ -10,7 +10,7 @@ public static class EquipmentUpgradeExtensions
 	public static List<EquipmentUpgradeDataModel> CanUpgradeEquipments(this IShipDataMaster ship, DayOfWeek day, List<EquipmentUpgradeDataModel> upgradesData)
 		=> upgradesData.Where(upgrade => upgrade.Improvement
 				.Any(improvement => improvement.Helpers
-					.Any(helpers => helpers.ShipIds.Contains(ship.ShipID))
+					.Any(helpers => helpers.ShipIds.Contains(ship.ShipID) && helpers.CanHelpOnDays.Contains(day))
 				)
 			)
 			.ToList();
