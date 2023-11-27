03f09fe712213eb91db035afab9839ca87a48908
Jebzou <22751386+Jebzou@users.noreply.github.com>
2023-11-27 22:31:30 +0100

    More test

--- ElectronicObserverCoreTests/FitBonus/TorpedoFitBonusTest.cs
+++ ElectronicObserverCoreTests/FitBonus/TorpedoFitBonusTest.cs
@@ -60,4 +60,30 @@ public class TorpedoFitBonusTest(DatabaseFixture db) : FitBonusTest(db)
 
 		Assert.Equal(expectedBonus, finalBonus);
 	}
+
+	[Fact(DisplayName = "21inch 4-tube Bow Torpedo Launcher (Initial Model) & (Late Model) on Scamp (Bonus don't stack)")]
+	public void TorpedoFitBonusTest3()
+	{
+		Assert.NotEmpty(BonusData.FitBonusList);
+
+		IShipData scamp = new ShipDataMock(Db.MasterShips[ShipId.ScampKai])
+		{
+			Level = 180,
+			SlotInstance = new List<IEquipmentData?>
+			{
+				new EquipmentDataMock(Db.MasterEquipment[EquipmentId.SubmarineTorpedo_21inch4tubeBowTorpedoLauncher_InitialModel]),
+				new EquipmentDataMock(Db.MasterEquipment[EquipmentId.SubmarineTorpedo_21inch4tubeBowTorpedoLauncher_LateModel])
+			}
+		};
+
+		FitBonusValue expectedBonus = new()
+		{
+			Torpedo = 1,
+			Evasion = 2
+		};
+
+		FitBonusValue finalBonus = scamp.GetTheoricalFitBonus(BonusData.FitBonusList);
+
+		Assert.Equal(expectedBonus, finalBonus);
+	}
 }
