9e66455968d050576eef2213c8ba3df75f0a12cd
Jebzou <22751386+Jebzou@users.noreply.github.com>
2024-03-25 22:33:08 +0100

    Scroll viewer & compact mode

--- ElectronicObserver/App.xaml.cs
+++ ElectronicObserver/App.xaml.cs
@@ -387,10 +387,10 @@ public partial class App : Application
 			.Property(w => w.ViewModel.Filters.DisplayFinished)
 			.Property(w => w.ViewModel.Filters.SelectAllDay)
 			.Property(w => w.ViewModel.Filters.SelectToday)
-			.Property(w => w.ViewModel.CompactMode)
+			.Property(w => w.ViewModel.EquipmentUpgradePlanManager.CompactMode)
 			.Property(w => w.ViewModel.PlanListWidth)
 			.Property(w => w.ViewModel.PlannedUpgradesPager.ItemsPerPage);
-
+		
 		tracker
 			.Configure<ExpCheckerWindow>()
 			.Property(w => w.ViewModel.DataGridViewModel.ColumnProperties)
