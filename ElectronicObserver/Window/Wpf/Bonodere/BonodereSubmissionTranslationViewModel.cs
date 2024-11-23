07ce3b7623684ff3b39f36c6a79530856fc70ba9
Jebzou <22751386+Jebzou@users.noreply.github.com>
2024-11-23 13:00:08 +0100

    tl

--- ElectronicObserver/Window/Wpf/Bonodere/BonodereSubmissionTranslationViewModel.cs
+++ ElectronicObserver/Window/Wpf/Bonodere/BonodereSubmissionTranslationViewModel.cs
@@ -5,7 +5,4 @@ namespace ElectronicObserver.Window.Wpf.Bonodere;
 public class BonodereSubmissionTranslationViewModel : TranslationBaseViewModel
 {
 	public string InconsistantDataDetected => BonodereSubmissionResources.InconsistantDataDetected;
-	public string Login => BonodereSubmissionResources.Login;
-	public string Logout => BonodereSubmissionResources.Logout;
-	public string SubmitData => BonodereSubmissionResources.SubmitData;
 }
---
+++ ElectronicObserver/Window/Wpf/SenkaLeaderboard/SenkaLeaderboardTranslationViewModel.cs
@@ -0,0 +1,9 @@
+﻿using ElectronicObserver.ViewModels.Translations;
+
+namespace ElectronicObserver.Window.Wpf.SenkaLeaderboard;
+
+public class SenkaLeaderboardTranslationViewModel : TranslationBaseViewModel
+{
+	public string LoadedEntries => SenkaLeaderboardResources.LoadedEntries;
+	public string SubmitData => SenkaLeaderboardResources.SubmitData;
+}
