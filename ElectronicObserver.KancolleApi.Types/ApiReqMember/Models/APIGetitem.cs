6fb778605394a73993081bc2d5cc7b68a967340e
MyAngelKamikaze <myangelkamikaze@gmail.com>
2024-11-25 00:25:59 +0900

    add comment

--- ElectronicObserver.KancolleApi.Types/ApiReqMember/Models/APIGetitem.cs
+++ ElectronicObserver.KancolleApi.Types/ApiReqMember/Models/APIGetitem.cs
@@ -10,6 +10,7 @@ public class ApiGetitem
 
 	/// <summary>
 	/// Element type is <see cref="Models.ApiSlotitem"/> or <see cref="List{T}"/> of <see cref="Models.ApiSlotitem"/>s.
+	/// It seems like the only instance of this being a list was when exchanging teru teru bozu for an antenna (equipment id 532), 2024/06/28 00:17:04 JST
 	/// </summary>
 	[JsonPropertyName("api_slotitem")]
 	public object? ApiSlotitem { get; set; }
