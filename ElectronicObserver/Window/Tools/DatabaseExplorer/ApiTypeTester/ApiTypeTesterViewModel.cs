b557daf8e5110f6e68b859793df31cd5cba5ebbb
MyAngelKamikaze <myangelkamikaze@gmail.com>
2024-11-24 23:57:54 +0900

    useless using

--- ElectronicObserver/Window/Tools/DatabaseExplorer/ApiTypeTester/ApiTypeTesterViewModel.cs
+++ ElectronicObserver/Window/Tools/DatabaseExplorer/ApiTypeTester/ApiTypeTesterViewModel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Linq;
-using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Web;
