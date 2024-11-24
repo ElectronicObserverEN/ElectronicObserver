628a1bd413da0ea33af121f9ae49d52beeebd840
Jebzou <22751386+Jebzou@users.noreply.github.com>
2024-11-24 08:48:07 +0100

    Columns & fix

--- ElectronicObserver/Window/Wpf/SenkaLeaderboard/SenkaEntryModel.cs
+++ ElectronicObserver/Window/Wpf/SenkaLeaderboard/SenkaEntryModel.cs
@@ -1,10 +1,21 @@
-﻿namespace ElectronicObserver.Window.Wpf.SenkaLeaderboard;
+﻿using System.Text.Json.Serialization;
+
+namespace ElectronicObserver.Window.Wpf.SenkaLeaderboard;
 
 public record SenkaEntryModel
 {
+	[JsonPropertyName("position")]
 	public required int Position { get; set; }
+
+	[JsonPropertyName("admiral")]
 	public required string AdmiralName { get; set; }
+
+	[JsonPropertyName("senka")]
 	public required int Points { get; set; }
+
+	[JsonPropertyName("medal")]
 	public required int MedalCount { get; set; }
+
+	[JsonPropertyName("comment")]
 	public required string Comment { get; set; }
 }
--- ElectronicObserver/Window/Wpf/SenkaLeaderboard/SenkaLeaderboardTranslationViewModel.cs
+++ ElectronicObserver/Window/Wpf/SenkaLeaderboard/SenkaLeaderboardTranslationViewModel.cs
@@ -7,4 +7,10 @@ public class SenkaLeaderboardTranslationViewModel : TranslationBaseViewModel
 	public string LoadedEntries => SenkaLeaderboardResources.LoadedEntries;
 	public string SubmitData => SenkaLeaderboardResources.SubmitData;
 	public string Title => SenkaLeaderboardResources.Title;
+
+	public string Rank => SenkaLeaderboardResources.Rank;
+	public string Admiral => SenkaLeaderboardResources.Admiral;
+	public string Senka => SenkaViewerResources.Senka;
+	public string Medal => HeadquartersResources.ItemNameFirstClassMedal;
+	public string AdmiralComment => SenkaLeaderboardResources.AdmiralComment;
 }
