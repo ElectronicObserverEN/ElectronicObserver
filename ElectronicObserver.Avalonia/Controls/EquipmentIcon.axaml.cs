7f35948b6cfef8b17e3dc7d9eab3357cdfbb767d
myangelkamikaze <40002167+myangelkamikaze@users.noreply.github.com>
2025-07-24 21:33:49 +0900

    add icons

---
+++ ElectronicObserver.Avalonia/Controls/EquipmentIcon.axaml.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Markup.Xaml;
+using ElectronicObserver.Core.Types;
+
+namespace ElectronicObserver.Avalonia.Controls;
+
+public partial class EquipmentIcon : UserControl
+{
+	public static readonly StyledProperty<EquipmentIconType> IconTypeProperty =
+		AvaloniaProperty.Register<EquipmentIcon, EquipmentIconType>(nameof(IconType), defaultBindingMode: BindingMode.OneWay);
+
+	public EquipmentIconType IconType
+	{
+		get => GetValue(IconTypeProperty);
+		set => SetValue(IconTypeProperty, value);
+	}
+	
+	public EquipmentIcon()
+	{
+		InitializeComponent();
+	}
+}
+
