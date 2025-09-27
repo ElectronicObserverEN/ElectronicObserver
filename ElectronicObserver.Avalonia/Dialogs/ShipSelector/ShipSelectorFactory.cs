db449e341ee1b0f35c454e18c229a02de56f1af5
myangelkamikaze <40002167+myangelkamikaze@users.noreply.github.com>
2025-09-27 15:17:29 +0900

    rework picker

---
+++ ElectronicObserver.Avalonia/Dialogs/ShipSelector/ShipSelectorFactory.cs
@@ -0,0 +1,32 @@
+﻿using ElectronicObserver.Avalonia.Services;
+using ElectronicObserver.Core.Services;
+using ElectronicObserver.Core.Types;
+using ElectronicObserver.Core.Types.Data;
+using ElectronicObserver.Core.Types.Mocks;
+
+namespace ElectronicObserver.Avalonia.Dialogs.ShipSelector;
+
+/// <summary>
+/// Needed because the ships get loaded after the trackers get initialized.
+/// </summary>
+public class ShipSelectorFactory(IKCDatabase db, TransliterationService transliterationService,
+	ImageLoadService imageLoadService)
+{
+	private IKCDatabase Db { get; } = db;
+	private TransliterationService TransliterationService { get; } = transliterationService;
+	private ImageLoadService ImageLoadService { get; } = imageLoadService;
+	public ShipSelectorViewModel ShipSelectorViewModel => field ??= CreateShipSelectorViewModel();
+
+	private ShipSelectorViewModel CreateShipSelectorViewModel()
+	{
+		List<IShipData> ships = Db.MasterShips.Values
+			.Select(s => new ShipDataMock(s))
+			.OfType<IShipData>()
+			.ToList();
+
+		return new(TransliterationService, ImageLoadService, ships)
+		{
+			ShipFilter = { FinalRemodel = false },
+		};
+	}
+}
