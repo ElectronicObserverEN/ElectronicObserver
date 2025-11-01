576a1cf9c8c742c70c6ea17c65cfbe5d830d6a52
Jebzou <22751386+Jebzou@users.noreply.github.com>
2025-11-01 06:07:59 +0100

    Add event names

--- ElectronicObserver/Utility/Data/TransportGaugeService.cs
+++ ElectronicObserver/Utility/Data/TransportGaugeService.cs
@@ -20,21 +20,26 @@ public class TransportGaugeService(IKCDatabase db, FormFleetOverviewTranslationV
 		// LastOrDefault is used here because in debug we add old event areas to KCDatabase. In release there's 0 or 1 event area.
 		if (KCDatabase.MapArea.Values.LastOrDefault(area => area.IsEventArea) is not { } eventArea) return "";
 
-		return GetEventLandingOperationToolTip(fleets, Enum.GetValues<TpGauge>().Where(gauge => gauge.GetGaugeAreaId() == eventArea.MapAreaID).ToList());
+		return GetEventLandingOperationToolTip(fleets, Enum.GetValues<TpGauge>().Where(gauge => gauge.GetGaugeAreaId() == eventArea.MapAreaID).ToList(), false);
 	}
 
 	public string GetAllEventLandingOperationToolTip(List<IFleetData> fleets)
 	{
-		return GetEventLandingOperationToolTip(fleets, Enum.GetValues<TpGauge>().Skip(2).ToList());
+		return GetEventLandingOperationToolTip(fleets, Enum.GetValues<TpGauge>().Skip(2).ToList(), true);
 	}
 
-	private string GetEventLandingOperationToolTip(List<IFleetData> fleets, List<TpGauge> gauges)
+	private string GetEventLandingOperationToolTip(List<IFleetData> fleets, List<TpGauge> gauges, bool displayEventName)
 	{
 		StringBuilder sb = new();
 
 		foreach (TpGauge gauge in gauges)
 		{
 			int tp = gauge.GetTp(fleets);
+
+			if (displayEventName)
+			{
+				sb.Append($"{GetEventName(gauge)} ");
+			}
 
 			sb.AppendLine($"E{gauge.GetGaugeMapId()}-{gauge.GetGaugeIndex()}: S {tp} / A {(int)(tp * 0.7)}");
 		}
@@ -43,4 +48,11 @@ public class TransportGaugeService(IKCDatabase db, FormFleetOverviewTranslationV
 
 		return $"\n{Translations.LandingOperationTooltip}:\n{sb.ToString().TrimEnd()}";
 	}
+
+	public static string GetEventName(TpGauge gauge) => gauge.GetGaugeAreaId() switch
+	{
+		60 => EventNames.Spring2025,
+		61 => EventNames.Fall2025,
+		_ => "",
+	};
 }
