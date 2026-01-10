3fd5d6d93a49f1f9e43c5e254354b1393a64ec64
myangelkamikaze <40002167+myangelkamikaze@users.noreply.github.com>
2026-01-10 14:26:33 +0900

    Remove CefSharp (#627)

--- Browser/BrowserView.xaml.cs
+++ Browser/BrowserView.xaml.cs
@@ -1,28 +1,20 @@
 ﻿using System.Windows;
 using System.Windows.Media;
 using Windows.UI.ViewManagement;
-using Browser.CefSharpBrowser;
 using Browser.WebView2Browser;
 using BrowserLibCore;
 
 namespace Browser;
 
-/// <summary>
-/// Interaction logic for BrowserView.xaml
-/// </summary>
 public partial class BrowserView
 {
 	public BrowserViewModel ViewModel { get; }
 	
-	public BrowserView(string host, int port, string culture, BrowserOption browser)
+	public BrowserView(string host, int port, string culture)
 	{
 		InitializeComponent();
 
-		ViewModel = browser switch
-		{
-			BrowserOption.WebView2 => new WebView2ViewModel(host, port, culture),
-			_ => new CefSharpViewModel(host, port, culture),
-		};
+		ViewModel =  new WebView2ViewModel(host, port, culture);
 
 		Loaded += ViewModel.OnLoaded;
 		DataContext = ViewModel;
