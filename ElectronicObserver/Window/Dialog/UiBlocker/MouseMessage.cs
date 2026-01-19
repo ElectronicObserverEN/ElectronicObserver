158fa2786c41a8fbada5c43d91d3aa36dfd9612f
myangelkamikaze <40002167+myangelkamikaze@users.noreply.github.com>
2026-01-19 12:38:31 +0900

    UI blocker (#630)

---
+++ ElectronicObserver/Window/Dialog/UiBlocker/MouseMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace ElectronicObserver.Window.Dialog.UiBlocker;
+
+public enum MouseMessage
+{
+	WM_LBUTTONDOWN = 0x0201,
+	WM_LBUTTONUP = 0x0202,
+	WM_MOUSEMOVE = 0x0200,
+	WM_RBUTTONDOWN = 0x0204,
+	WM_RBUTTONUP = 0x0205,
+}
