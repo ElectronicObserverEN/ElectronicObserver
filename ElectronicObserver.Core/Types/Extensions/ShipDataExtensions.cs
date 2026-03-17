c833bd394d4cf702f258d0ee912f0f3a97f6769b
myangelkamikaze <40002167+myangelkamikaze@users.noreply.github.com>
2026-03-18 01:26:54 +0900

    remove dupe code

--- ElectronicObserver.Core/Types/Extensions/ShipDataExtensions.cs
+++ ElectronicObserver.Core/Types/Extensions/ShipDataExtensions.cs
@@ -122,9 +122,10 @@ public static class ShipDataExtensions
 		.Zip(ship.Aircraft, (e, size) => (e, size))
 		.Any(s => s.size > 0 && s.e?.MasterEquipment.CategoryType == EquipmentTypes.JetFighter);
 
-	public static bool HasJetBomber(this IShipData ship) => ship.AllSlotInstance
+	public static bool HasJetBomber(this IShipData ship, int count = 1) => ship.AllSlotInstance
 		.Zip(ship.Aircraft, (e, size) => (e, size))
-		.Any(s => s.size > 0 && s.e?.MasterEquipment.CategoryType == EquipmentTypes.JetBomber);
+		.Count(s => s.size > 0 && s.e?.MasterEquipment.CategoryType is EquipmentTypes.JetBomber)
+		>= count;
 
 	public static bool HasFighter(this IShipData ship) => ship.AllSlotInstance
 		.Zip(ship.Aircraft, (e, size) => (e, size))
@@ -138,12 +139,6 @@ public static class ShipDataExtensions
 	public static bool HasAttacker(this IShipData ship) => ship.AllSlotInstance
 		.Zip(ship.Aircraft, (e, size) => (e, size))
 		.Any(s => s.size > 0 && s.e?.MasterEquipment.CategoryType == EquipmentTypes.CarrierBasedTorpedo);
-
-	public static bool HasJetBomber(this IShipData ship, int count = 1) =>
-		ship.AllSlotInstance
-			.Zip(ship.Aircraft, (e, size) => (e, size))
-			.Count(s => s.size > 0 && s.e?.MasterEquipment.CategoryType is EquipmentTypes.JetBomber)
-		>= count;
 
 	public static bool HasTorpedo(this IShipData ship, int count = 1) => ship.AllSlotInstance
 		.Count(e => e?.MasterEquipment.IsTorpedo == true) >= count;
