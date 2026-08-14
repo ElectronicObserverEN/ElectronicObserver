7d0bae58e28054caba57b3a174c75cb7ad06be65
Jebzou <22751386+Jebzou@users.noreply.github.com>
2026-08-14 06:57:46 +0200

    Escort check

--- ElectronicObserver/Utility/Data/FleetSpecialAttacks.cs
+++ ElectronicObserver/Utility/Data/FleetSpecialAttacks.cs
@@ -9,19 +9,30 @@ public static class FleetSpecialAttacks
 {
 	public static List<SpecialAttack> GetSpecialAttacks(this IFleetData fleet)
 	{
-		List<SpecialAttack> attacks =
-		[
-			new NelsonSpecialAttack(fleet),
-			new NagatoSpecialAttack(fleet),
-			new ColoradoSpecialAttack(fleet),
-			new Yamato123SpecialAttack(fleet),
-			new Yamato12SpecialAttack(fleet),
-			new KongouSpecialAttack(fleet),
-			new SubmarineSpecialAttack(fleet),
-			new RichelieuSpecialAttack(fleet),
-			new QueenElizabethSpecialAttack(fleet),
-		];	
+		List<SpecialAttack> attacks = [];
 
+		if (fleet.FleetType is FleetType.Single || fleet.FleetID == 1)
+		{
+			attacks = [
+				new NelsonSpecialAttack(fleet),
+				new NagatoSpecialAttack(fleet),
+				new ColoradoSpecialAttack(fleet),
+				new Yamato123SpecialAttack(fleet),
+				new Yamato12SpecialAttack(fleet),
+				new KongouSpecialAttack(fleet),
+				new SubmarineSpecialAttack(fleet),
+				new RichelieuSpecialAttack(fleet),
+				new QueenElizabethSpecialAttack(fleet),
+			];
+		}
+		else
+		{
+			attacks = [
+				new KongouSpecialAttack(fleet),
+				new SubmarineSpecialAttack(fleet),
+			];
+		}
+		
 		return attacks
 			.Where(attack => attack.CanTrigger())
 			.ToList();
